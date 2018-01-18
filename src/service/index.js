ddb8a6ee6072801c95767f318be13f8018213fd3
liushuang <liushuang@gegejia.com>
2018-01-18 19:03:53 +0800

    service封装

--- src/service/index.js
+++ src/service/index.js
@@ -13,10 +13,6 @@ export default const service = (url, option = {})=>{
 		res.json().then(response=>{
 			if(!response){
 				throw '服务器返回参数错误'
-			}else if(response.errcode == 40001){
-				throw "token失效，请刷新页面"
-			}else if(response.errcode == -1){
-				return response
 			}
 			return response;
 		})
@@ -43,5 +39,63 @@ export default const service = (url, option = {})=>{
 		default:
 			break;
 	}
+	param.cache = 'reload';
+	param.mode = 'same-origin';
 	return fetch(url,params).then(callback).catch(errHandle);
+}
+
+//todo
+const get = function(url, params, headers) {
+  if (params) {
+    let paramsArray = [];
+    //encodeURIComponent
+    Object.keys(params).forEach(key => paramsArray.push(key + '=' + params[key]))
+    if (url.search(/\?/) === -1) {
+      url += '?' + paramsArray.join('&')
+    } else {
+      url += '&' + paramsArray.join('&')
+    }
+  }
+  return new Promise(function (resolve, reject) {
+   fetch(url, {
+      method: 'GET',
+      headers: headers,
+     })
+     .then((response) => {
+       if (response.ok) {
+         return response.json();
+       } else {
+         reject({status:response.status})
+       }
+     })
+     .then((response) => {
+       resolve(response);
+     })
+     .catch((err)=> {
+      reject({status:-1});
+     })
+  })
+}
+
+const post = function(url, formData, headers) {
+  return new Promise(function (resolve, reject) {
+   fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body:formData,
+     })
+     .then((response) => {
+       if (response.ok) {
+         return response.json();
+       } else {
+         reject({status:response.status})
+       }
+     })
+     .then((response) => {
+       resolve(response);
+     })
+     .catch((err)=> {
+      reject({status:-1});
+     })
+  })
 }
