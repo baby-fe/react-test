99de76f077d42e857ae2461edd5d7b3309ab4f56
liushuang <liushuang@gegejia.com>
2018-01-31 20:43:17 +0800

    架构调整

--- src/containers/home/route.js
+++ src/containers/home/route.js
@@ -6,10 +6,12 @@ import reducerHome from './reducers/reducer-home'
 import { key_home } from '@/constants'
 
 const loader = (store, nextState, cb, module, key, rdcr) => {
-    const originState = store.getState()
-    const newState = originState.get(key) ? originState : originState.set(key,Immutable.Map({}))
-    store.reset(combineReducers({...store._reducers,[key]: rdcr}), newState)
-        cb(null, module.default);
+    if(key && rdcr){
+        const originState = store.getState()
+        const newState = originState.get(key) ? originState : originState.set(key,Immutable.Map({}))
+        store.reset(combineReducers({...store._reducers,[key]: rdcr}), newState)
+    }
+    cb(null, module.default);
 }
 //异步加载test模块，重置store
 const homeLoader = (store, nextState, cb) => {
--- src/containers/test/route.js
+++ src/containers/test/route.js
@@ -6,10 +6,12 @@ import reducerTest from './reducers/reducer-test'
 import { key_test, key_test_inner} from '@/constants'
 
 const loader = (store, nextState, cb, module, key, rdcr) => {
-    const originState = store.getState()
-    const newState = originState.get(key) ? originState : originState.set(key,Immutable.Map({}))
-    store.reset(combineReducers({...store._reducers,[key]: rdcr}), newState)
-        cb(null, module.default);
+    if(key && rdcr){
+        const originState = store.getState()
+        const newState = originState.get(key) ? originState : originState.set(key,Immutable.Map({}))
+        store.reset(combineReducers({...store._reducers,[key]: rdcr}), newState)
+    }
+    cb(null, module.default);
 }
 //异步加载test模块，重置store
 const testLoader = (store, nextState, cb) => {
